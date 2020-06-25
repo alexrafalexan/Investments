1a78e64199305931c56583d8ec7a79a4bb34317f
alexrafalexan <fordfocus88aa>
2020-06-25 22:41:21 +0300

    semantic-ui-css

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import creator from '../ethproject/creator';
+import {Card} from "semantic-ui-react";
+
 
 class InvestmentIndex extends Component {
-    static async getInitialProps(){
+    static async getInitialProps() {
         const investments = await creator.methods.getListOfCreatedInvestments().call();
         return {investments};
     }
+
+    renderInvestments() {
+        const items = this.props.investments.map(address => {
+            return {
+                header: address,
+                description: <a>Details of Investment</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items}/>
+    }
+
     render() {
-        return <div>{this.props.investments[0]}</div>
+        return <div>
+            <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+            {this.renderInvestments()}
+        </div>
     }
 }
+
 
 
 export default InvestmentIndex;
