77a5829a7a34fad83f1e89720e9b6eb6fe62e2a0
alexrafalexan <fordfocus88aa>
2020-06-25 14:39:28 +0300

    Compiled to the Rinkeby network

--- ethproject/factory.js
+++ ethproject/factory.js
@@ -1,3 +1,9 @@
 import web3 from './web3';
 import Application from './build/Application.json';
 
+const inst = new web3.eth.Contract(
+    JSON.parse(Application.interface),
+    '0x6B0A46E2E74d4D3739A64060ce573c8650546a90'
+);
+
+export default  inst;
