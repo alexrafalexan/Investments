25d5064a16ce1a03de3b3fd1b66dab354fba9d89
alexrafalexan <fordfocus88aa>
2020-06-25 10:20:48 +0300

    First Test Success

--- test/Application.test.js
+++ test/Application.test.js
@@ -15,15 +15,22 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     application = await new web3.eth.Contract(JSON.parse(compiledApplication.interface))
         .deploy({data: compiledApplication.bytecode})
-        .send({from:accounts[0], gas: '1000000'})
+        .send({from:accounts[0], gas: '4712388'})
 
     await application.methods.createInvestment('2','2','3600','100','20','3').send({
        from: accounts[0],
-       gas: '1000000'
+       gas: '4712388'
     });
 
     const _investmentAddress = await application.methods.getListOfCreatedInvestments().call();
     investmentAddress = _investmentAddress[0];
 
     investment =await new web3.eth.Contract(JSON.parse(compiledInvestment.interface), investmentAddress);
+});
+
+describe('Investment', () =>{
+    it('Create Investment from Application Contract', () => {
+        assert.ok(application.options.address);
+        assert.ok(investment.options.address);
+    });
 });
