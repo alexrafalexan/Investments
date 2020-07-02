9413a2b41feab064aadb4cf19c37cb3af58fc657
alexrafalexan <fordfocus88aa>
2020-07-02 10:36:35 +0300

    CHANGE ADDRESS.txt

--- ethproject/creator.js
+++ ethproject/creator.js
@@ -3,7 +3,7 @@ import Application from './build/Application.json';
 
 const inst = new web3.eth.Contract(
     JSON.parse(Application.interface),
-    '0xd525c5fE55D593dac9FF71AaEC07ed35726704d2'
+    '0x0A266532981bb0b1E53E257560CdA85419663e80'
 );
 
 export default  inst;
